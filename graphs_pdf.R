29b3115b3be49d530fdf9300275d3172fb00a7b4
threndash <grendash@gmail.com>
2025-06-23 19:31:28 +0200

    upd

--- graphs_pdf.R
+++ graphs_pdf.R
@@ -12,3 +12,6 @@ if(!dir.exists("graphs_mod/files")){dir.create("graphs_mod/files")}
 system(paste("7z a ",paste0("graphs_mod/files/",zip_file),"graphs_pdf/."))
 
 system("scp -r graphs_mod/files root@194.182.65.144:/srv/shiny-server/zamestnancistatu_2025/")
+
+unlink(x = paste0("graphs_mod/files/",list.files("graphs_mod/files")))
+unlink(x = paste0("graphs_pdf/",list.files("graphs_pdf/")))
