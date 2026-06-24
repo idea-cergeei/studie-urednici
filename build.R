1e5bcc433c6f09e93fd753b6608cdaf0a7606170
Petr Bouchal <pbouchal@gmail.com>
2026-06-24 22:31:36 +0200

    PDF packaging

--- build.R
+++ build.R
@@ -1,3 +1,5 @@
+library(purrr)
+
 # see howto-update-nextyear.md
 
 source("standardize_input_data.R", local = new.env())
@@ -23,6 +25,29 @@ source("graphs.R", local = new.env())
 source("dashboard.R", local = new.env())
 source("graphs_mod.R", local = new.env())
 source("graphs_mod_long.r", local = new.env())
+
+# Convert individual numbered graphs to PDF using Chrome headless
+chrome <- "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
+graf_htmls <- list.files("graphs_mod", pattern = "^graf_[A-Z0-9]+\\.html$", full.names = TRUE)
+pdf_dir <- "graphs_mod/pdf/grafy"
+dir.create(pdf_dir, showWarnings = FALSE, recursive = TRUE)
+walk(graf_htmls, \(html) {
+  pdf_out <- file.path(pdf_dir, sub("\\.html$", ".pdf", basename(html)))
+  system2(chrome, args = c(
+    "--headless", "--no-sandbox", "--disable-gpu",
+    paste0("--print-to-pdf=", normalizePath(pdf_out, mustWork = FALSE)),
+    normalizePath(html, mustWork = TRUE)
+  ), stdout = FALSE, stderr = FALSE)
+})
+
+# Zip the PDFs into graphs_mod/files/Grafy.zip
+files_dir <- "graphs_mod/files"
+dir.create(files_dir, showWarnings = FALSE, recursive = TRUE)
+zip_path <- file.path(files_dir, "Grafy.zip")
+pdf_files <- list.files(pdf_dir, pattern = "\\.pdf$", full.names = TRUE)
+zip(zipfile = normalizePath(zip_path, mustWork = FALSE),
+    files   = normalizePath(pdf_files, mustWork = TRUE))
+
 # system("scp -r graphs_mod/* root@194.182.65.144:/srv/shiny-server/zamestnancistatu")
 # unlink(x = paste0("graphs/",grep("graf",list.files("graphs"),value = T)))
 
